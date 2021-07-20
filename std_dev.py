7df0475986b769f072fae926279b91d196f4192c
HighWarlordRage <hussaias@oregonstate.edu>
2021-07-20 06:40:55 +0500

    Initial commit

---
+++ std_dev.py
@@ -0,0 +1,32 @@
+#Title: Project-6c
+class Person():
+    name = ""
+    age = 0
+
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def get_age(self):
+        """a function that takes as a parameter a list of Person objects and
+        returns the standard deviation of all their ages"""
+        return self.__age
+
+def std_dev(person_list):
+    total = 0
+    for person in person_list:
+        total += person.get_age()
+    mean_age = total / len(person_list)
+
+    square_sum = 0
+    for person in person_list:
+        square_sum += (mean_age - person.get_age()) ** 2
+
+    return (square_sum / len(person_list)) ** 0.5
+
+p1 = Person("Kyoungmin", 73)
+p2 = Person("Mercedes", 24)
+p3 = Person("Beatrice", 48)
+person_list = [p1, p2, p3]
+answer = std_dev(person_list)
+#print('Standard Deviation of age:',answer)
